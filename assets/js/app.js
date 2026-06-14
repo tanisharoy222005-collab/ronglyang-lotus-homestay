7e83b0252820632d988e4360c911f7da79895a5a
tanisharoy222005-collab <tanisharoy222005@gmail.com>
2026-06-14 22:04:53 +0530

    Create app.js

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+console.log("Ronglyang Lotus Homestay Website Loaded");
+
+document.addEventListener("DOMContentLoaded", () => {
+
+const year = new Date().getFullYear();
+
+console.log("Current Year:", year);
+
+});
